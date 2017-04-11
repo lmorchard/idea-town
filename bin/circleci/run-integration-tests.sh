be4154dc2fa6802354172734f65638a130624fb5
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-04-11 14:19:14 -0700

    Updated tox call

--- bin/circleci/run-integration-tests.sh
+++ bin/circleci/run-integration-tests.sh
@@ -18,7 +18,7 @@ until $(curl --output /dev/null --silent --head --fail -k https://example.com:80
 done
 
 # Fire up the integration tests with Marionette
-tox
+tox --firefox-path=firefox/firefox
 TEST_STATUS=$?
 
 kill $STATIC_SERVER_PID
