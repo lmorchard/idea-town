b78ef64c381090635dc2085ca43ea31ad4f142a8
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-04-12 13:07:00 -0700

    Changed tox version

--- frontend/test/ui/conftest.py
+++ frontend/test/ui/conftest.py
@@ -13,14 +13,3 @@ def capabilities(capabilities):
     capabilities['marionette'] = True
     capabilities['acceptInsecureCerts'] = True
     return capabilities
-
-
-@pytest.fixture
-def firefox_profile(firefox_profile):
-    # firefox_profile.set_preference('accept_untrusted_certs', 'true')
-    firefox_profile.set_preference(
-        'extensions.install.requireBuiltInCerts', 'false')
-    firefox_profile.set_preference('xpinstall.signatures.required', 'false')
-    firefox_profile.set_preference('extensions.webapi.testing', 'false')
-    firefox_profile.set_preference('testpilot.dev', 'local')
-    return firefox_profile
