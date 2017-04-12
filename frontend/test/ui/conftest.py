6558ec497f70a7b09ca57df6d1c4d56bbed5b60f
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-04-12 10:21:00 -0700

    Attempted fix for https error

--- frontend/test/ui/conftest.py
+++ frontend/test/ui/conftest.py
@@ -11,3 +11,13 @@ def foxpuppet(selenium):
 @pytest.fixture
 def capabilities(capabilities):
     capabilities['marionette'] = True
+
+
+@pytest.fixture
+def firefox_profile(firefox_profile):
+    firefox_profile.set_preference(
+        'extensions.install.requireBuiltInCerts', 'false')
+    firefox_profile.set_preference('xpinstall.signatures.required', 'false')
+    firefox_profile.set_preference('extensions.webapi.testing', 'false')
+    firefox_profile.set_preference('example.com', 'local')
+    return firefox_profile
