aaf411af69460f9a4afabde00ad8e1b072dccf99
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-04-18 11:29:47 -0700

    Removed sauce labs debugs

--- bin/circleci/run-integration-tests.sh
+++ bin/circleci/run-integration-tests.sh
@@ -12,8 +12,6 @@
 npm start &
 STATIC_SERVER_PID=$!
 
-while [ ! -e ~/sauce_is_ready ]; do sleep 1; done
-
 # Wait until the server is available...
 until $(curl --output /dev/null --silent --head --fail -k https://example.com:8000/); do
     printf '.'; sleep 1
