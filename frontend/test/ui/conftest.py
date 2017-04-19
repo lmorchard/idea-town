5fb6be42dda0b5655283e312be9018e238d72d00
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-04-19 13:39:44 -0700

    Removed changes to conftest

--- frontend/test/ui/conftest.py
+++ frontend/test/ui/conftest.py
@@ -12,8 +12,6 @@ def foxpuppet(selenium):
 def capabilities(capabilities):
     capabilities['marionette'] = True
     capabilities['acceptInsecureCerts'] = True
-    capabilities['platform'] = 'Windows 10'
-    capabilities['version'] = 'dev'
     return capabilities
 
 
