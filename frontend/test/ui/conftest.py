88b1f50680aac7b89553b2a700005f79265c6b34
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-04-24 11:24:44 -0700

    Removed a capability for debugging

--- frontend/test/ui/conftest.py
+++ frontend/test/ui/conftest.py
@@ -11,7 +11,6 @@ def foxpuppet(selenium):
 @pytest.fixture
 def capabilities(capabilities):
     capabilities['marionette'] = True
-    capabilities['acceptInsecureCerts'] = True
     return capabilities
 
 
@@ -21,4 +20,5 @@ def firefox_options(firefox_options):
         'extensions.install.requireBuiltInCerts', False)
     firefox_options.set_preference('xpinstall.signatures.required', False)
     firefox_options.set_preference('extensions.webapi.testing', True)
+    firefox_options.set_preference('example.com', 'local')
     return firefox_options
